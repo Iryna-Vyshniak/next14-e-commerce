67b860f89515c759cf7222b1c12ee501137e5e00
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-29 23:25:59 +0200

    feat: Complete Footer component with Payload CMS

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -24,3 +24,27 @@ export const profileNavItems = [
         icon: '/assets/icons/logout.svg',
     },
 ]
+
+export const inclusions = [
+    {
+        title: 'Free Shipping',
+        description: 'Free shipping for order above $150',
+        icon: '/assets/icons/shipping.svg',
+    },
+    {
+        title: 'Money Guarantee',
+        description: 'Within 30 days for an exchange',
+        icon: '/assets/icons/dollar.svg',
+    },
+    {
+        title: 'Online Support',
+        description: '24 hours a day, 7 days a week',
+        icon: '/assets/icons/support.svg',
+    },
+    {
+        title: 'Flexible Payment',
+        description: 'Pay with multiple credit cards',
+        icon: '/assets/icons/payment.svg',
+    },
+]
+
