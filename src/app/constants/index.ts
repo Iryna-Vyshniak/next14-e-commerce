7394ff8e7529a7fb6d720db045430d6124a23a8a
Iryna-Vyshniak <iryna.vyshniak@gmail.com>
2024-01-29 22:08:50 +0200

    feat: Add constants for URLs and profile navigation items

---
+++ src/app/constants/index.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
+
+export const profileNavItems = [
+    {
+        title: 'Personal Information',
+        url: '/account',
+        icon: '/assets/icons/user.svg',
+    },
+    {
+        title: 'My Purchases',
+        url: '/account/purchases',
+        icon: '/assets/icons/purchases.svg',
+    },
+    {
+        title: 'My Orders',
+        url: '/account/orders',
+        icon: '/assets/icons/orders.svg',
+    },
+    {
+        title: 'Logout',
+        url: '/logout',
+        icon: '/assets/icons/logout.svg',
+    },
+]
